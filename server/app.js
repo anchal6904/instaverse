3f78e336ec0498e97a815fe6b94413846ff6b8d9
Anchal Jain <anchal6904@gmail.com>
2025-03-28 21:22:48 +0530

    Updated feature

--- server/app.js
+++ server/app.js
@@ -8,7 +8,7 @@ import userRoutes from './routes/users.js';
 dotenv.config();
 
 const app = express();
-app.use(cors());
+app.use(cors({ origin: "*" }));
 app.use(bodyParser.json({ limit: "32mb", extended: true }));
 app.use(bodyParser.urlencoded({ limit: "32mb", extended: true }));
 app.use('/stories', storyRoutes);
